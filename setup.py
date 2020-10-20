f5b58e93d902b70ed4f682b4ee4bf6e0fd306fda
jameschapman <james.chapman.19@ucl.ac.uk>
2020-10-20 13:52:45 +0100

    small change to example main and remove idea from git

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from setuptools import setup
 
 setup(
-    name='CCA_methods',
+    name='pyCCA',
     version='1',
-    packages=['CCA_methods'],
-    url='',
-    license='',
+    packages=['PyCCA'],
+    url='https://github.com/jameschapman19/CCA_methods',
+    license='MIT',
     author='jameschapman',
     author_email='james.chapman.19@ucl.ac.uk',
-    description=''
-)
+    description='',
+    install_requires=['matplotlib==3.1.1',
+                        'numpy==1.17.4',
+                        'pandas==1.0.3',
+                        'scikit-learn==0.22.1',
+                        'scipy==1.4.1',
+                        'seaborn==0.10.1',
+                        'torch==1.5.0',
+                        'torchvision==0.6.0a0+82fd1c8',
+                        'tqdm==4.47.0'])
