063436f9c1e5c16575fc6cb0b512c301f541ef6c
jameschapman <james.chapman.19@ucl.ac.uk>
2020-10-20 12:16:59 +0100

    small change to example main and remove idea from git

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CCA_methods',
+    version='1',
+    packages=['CCA_methods'],
+    url='',
+    license='',
+    author='jameschapman',
+    author_email='james.chapman.19@ucl.ac.uk',
+    description=''
+)
