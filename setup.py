8f73c58feff06d2e3c08ea946a445c897921138d
jameschapman <james.chapman.19@ucl.ac.uk>
2020-10-20 18:23:23 +0100

    v3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='cca_zoo',
-    version='1.0.2',
+    version='1.0.3',
     packages=find_packages(),
     url='https://github.com/jameschapman19/cca_zoo',
     license='MIT',
