0944c1aeb203899fdc12f169a2f201d44d23678a
jameschapman <james.chapman.19@ucl.ac.uk>
2020-10-20 18:31:46 +0100

    v3

--- cca_zoo/__init__.py
+++ cca_zoo/__init__.py
@@ -1,11 +1,11 @@
-__all__ = ["alternating_least_squares",
-           "DCCAE",
-           "DVCCA",
-           "KCCA",
-           "deep_models",
-           "deep",
-           "linear",
-           "mnist_utils",
-           "objectives",
-           "plot_utils",
-           "objectives"]
+import cca_zoo.linear
+import cca_zoo.objectives
+import cca_zoo.plot_utils
+import cca_zoo.mnist_utils
+import cca_zoo.alternating_least_squares
+import cca_zoo.deep
+import cca_zoo.deep_models
+import cca_zoo.KCCA
+import cca_zoo.generate_data
+import cca_zoo.DCCAE
+import cca_zoo.DVCCA
