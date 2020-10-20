17519a9b8dae68d790dcd5633a3bc8887faa0210
jameschapman <james.chapman.19@ucl.ac.uk>
2020-10-20 18:03:15 +0100

    Correcting the init

--- cca_zoo/__init__.py
+++ cca_zoo/__init__.py
@@ -1,1 +1,11 @@
-
+__all__ = ["alternating_least_squares",
+           "DCCAE",
+           "DVCCA",
+           "KCCA",
+           "deep_models",
+           "deep",
+           "linear",
+           "mnist_utils",
+           "objectives",
+           "plot_utils",
+           "objectives"]
