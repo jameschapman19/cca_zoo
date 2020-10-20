a8d11b540121518079961e55f75b751a6362d9a5
jameschapman <james.chapman.19@ucl.ac.uk>
2020-10-20 17:47:24 +0100

    Change version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='cca_zoo',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     url='https://github.com/jameschapman19/cca_zoo',
     license='MIT',
