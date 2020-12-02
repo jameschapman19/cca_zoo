f5ef0237b0e11d103a9c951612ab09cafabab6f9
jameschapman <james.chapman.19@ucl.ac.uk>
2020-12-02 18:03:04 +0000

    Making first docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
-
+master_doc = 'index'
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
