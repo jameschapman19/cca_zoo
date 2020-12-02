093b5f45682c8ec8ab7e3d76c6e5c5923b001d47
jameschapman <james.chapman.19@ucl.ac.uk>
2020-12-02 18:45:18 +0000

    Making first docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,15 +5,19 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
-master_doc = 'index'
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
 # sys.path.insert(0, os.path.abspath('.'))
+# The master toctree document.
 
+# The suffix of source filenames.
+source_suffix = '.rst'
+
+master_doc = 'index'
 
 # -- Project information -----------------------------------------------------
 
