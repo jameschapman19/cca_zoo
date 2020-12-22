641aec10e2d3b3fe5a4f53a7c3d8c683b69b77eb
James Chapman <james.chapman.19@ucl.ac.uk>
2020-12-22 13:24:28 +0000

    Adding travis CI

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-requirements=['numpy~=1.19.3',
- 'torch~=1.7.1',
-'scikit-learn~=0.23.2',
- 'scipy~=1.5.4',
- 'matplotlib~=3.3.3',
- 'seaborn~=0.11.0',
- 'Pillow~=8.0.1',
- 'torchvision~=0.8.2',
- 'pandas~=1.1.5',
- 'mvlearn~=0.4.1']
+requirements=['numpy',
+ 'torch',
+'scikit-learn',
+ 'scipy',
+ 'matplotlib',
+ 'seaborn',
+ 'Pillow',
+ 'torchvision',
+ 'pandas',
+ 'mvlearn']
 
 setup(
     name='cca_zoo',
