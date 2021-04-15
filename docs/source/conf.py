01d67d07e564df7ba0fee0a7efade33563caa780
James Chapman <james.chapman.19@ucl.ac.uk>
2021-04-15 13:27:38 +0100

    updating docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../cca_zoo'))
+sys.path.insert(0, os.path.abspath('../cca_zoo/'))
 
 # -- Project information -----------------------------------------------------
 
