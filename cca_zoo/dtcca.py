6da6b9971be87ff7ef175d371048ff6b57f1c40e
James Chapman <james.chapman.19@ucl.ac.uk>
2021-04-16 00:39:38 +0100

    updating docs

--- cca_zoo/dtcca.py
+++ cca_zoo/dtcca.py
@@ -1,4 +1,4 @@
-from typing import Iterable
+from typing import Iterable, Tuple
 
 import torch
 
@@ -16,7 +16,7 @@ class DTCCA(DCCA, torch.nn.Module):
     >>> model = DTCCA()
     """
 
-    def __init__(self, latent_dims: int, encoders: Iterable[BaseEncoder, ...] = (Encoder, Encoder),
+    def __init__(self, latent_dims: int, encoders: Tuple[BaseEncoder, ...] = (Encoder, Encoder),
                  learning_rate=1e-3, r: float = 0,
                  schedulers: Iterable = None, optimizers: Iterable = None):
         super().__init__(latent_dims, objective=TCCA, encoders=encoders, learning_rate=learning_rate, r=r,
