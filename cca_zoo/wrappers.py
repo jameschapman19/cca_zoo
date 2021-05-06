99b32264de4790e83689aa73328070b2153b488f
James Chapman <james.chapman.19@ucl.ac.uk>
2021-05-06 11:24:31 +0100

    Significant change to kernel CCA to allow callable kernels, multiple (more than 2 view) kernel CCA. Removed separate kcca.py. Simplified codebase and added functionality - Nice!

--- cca_zoo/wrappers.py
+++ cca_zoo/wrappers.py
@@ -269,26 +269,11 @@ class KCCA(MCCA):
         """
         :param latent_dims: number of latent dimensions
         :param c: list of regularisation parameters for each view (between 0:CCA and 1:PLS)
-        :param kernel: list of kernel mappings used internally. This parameter is directly passed to
-        :class:`~sklearn.metrics.pairwise.pairwise_kernel`.
-        If element of `kernel` is a string, it must be one of the metrics
-        in `pairwise.PAIRWISE_KERNEL_FUNCTIONS`.
-        Alternatively, if element of `kernel` is a callable function, it is called on
-        each pair of instances (rows) and the resulting value recorded. The
-        callable should take two rows from X as input and return the
-        corresponding kernel value as a single number. This means that
-        callables from :mod:`sklearn.metrics.pairwise` are not allowed, as
-        they operate on matrices, not single samples. Use the string
-        identifying the kernel instead.
-        :param gamma: list of gamma parameters for the RBF, laplacian, polynomial, exponential chi2
-        and sigmoid kernels. Interpretation of the default value is left to
-        the kernel; see the documentation for sklearn.metrics.pairwise.
-        Ignored by other kernels.
+        :param kernel: list of kernel mappings used internally. This parameter is directly passed to :class:`~sklearn.metrics.pairwise.pairwise_kernel`. If element of `kernel` is a string, it must be one of the metrics in `pairwise.PAIRWISE_KERNEL_FUNCTIONS`. Alternatively, if element of `kernel` is a callable function, it is called on each pair of instances (rows) and the resulting value recorded. The callable should take two rows from X as input and return the corresponding kernel value as a single number. This means that callables from :mod:`sklearn.metrics.pairwise` are not allowed, as they operate on matrices, not single samples. Use the string identifying the kernel instead.
+        :param gamma: list of gamma parameters for the RBF, laplacian, polynomial, exponential chi2 and sigmoid kernels. Interpretation of the default value is left to the kernel; see the documentation for sklearn.metrics.pairwise. Ignored by other kernels.
         :param degree: list of degree parameters of the polynomial kernel. Ignored by other kernels.
-        :param coef0: list of zero coefficients for polynomial and sigmoid kernels.
-        Ignored by other kernels.
-        :param kernel_params: list of additional parameters (keyword arguments) for kernel function passed
-        as callable object.
+        :param coef0: list of zero coefficients for polynomial and sigmoid kernels. Ignored by other kernels.
+        :param kernel_params: list of additional parameters (keyword arguments) for kernel function passed as callable object.
         :param eps: epsilon value to ensure stability
         """
         super().__init__(latent_dims=latent_dims)
