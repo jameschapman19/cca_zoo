1159c42985cfdfb56bb2a5c16858af32962d10c9
James Chapman <james.chapman.19@ucl.ac.uk>
2021-05-18 09:34:04 +0100

    Realised I wasn't copying arrays used to fit the model. Something if a quiet bug but now works in a similar way to sklearn i.e. copying input arrays by default

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("./requirements/probablistic.txt", "r") as f:
 
 setup(
     name='cca_zoo',
-    version='1.4.4',
+    version='1.5.0',
     include_package_data=True,
     keywords='cca',
     packages=find_packages(),
