ca85e29901827cbf5df3b95975150039f6946005
James Chapman <james.chapman.19@ucl.ac.uk>
2021-06-15 12:59:03 +0100

    V1.6

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("./requirements/probablistic.txt", "r") as f:
 
 setup(
     name='cca_zoo',
-    version='1.6.1',
+    version='1.6.2',
     include_package_data=True,
     keywords='cca',
     packages=find_packages(),
