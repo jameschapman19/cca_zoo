d0e482d002e0ef2e947819380853c11d99ebd638
James Chapman <james.chapman.19@ucl.ac.uk>
2021-06-24 18:49:27 +0100

    Shift optimizer stuff out of models and  into the DeepWrapper class. Much tidier. Much more flexible.

--- cca_zoo/deepmodels/deepwrapper.py
+++ cca_zoo/deepmodels/deepwrapper.py
@@ -250,4 +250,5 @@ class DeepWrapper(_CCA_Base):
                 val_batch_size = len(val_dataset)
             val_dataloader = DataLoader(val_dataset, batch_size=val_batch_size, drop_last=True)
             _check_batch_size(batch_size, self.latent_dims)
-        return train_dataloader, val_dataloader
+            return train_dataloader, val_dataloader
+        return train_dataloader
