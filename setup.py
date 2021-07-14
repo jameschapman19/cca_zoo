66552c91311e18c275e028a7a23b7cf8d1520b7e
James Chapman <james.chapman.19@ucl.ac.uk>
2021-07-14 18:09:11 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("./requirements/probablistic.txt", "r") as f:
 
 setup(
     name='cca_zoo',
-    version='1.7.9',
+    version='1.7.10',
     include_package_data=True,
     keywords='cca',
     packages=find_packages(),
