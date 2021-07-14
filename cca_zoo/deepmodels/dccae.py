Fixes after double/float issue. Changed DCCAE to mean for better scaling

cca_zoo/deepmodels/dccae.py
@@ -62,5 +62,5 @@ class DCCAE(_DCCA_base):
 
     @staticmethod
     def recon_loss(x, recon):
-        recons = [F.mse_loss(recon_, x_, reduction='sum') for recon_, x_ in zip(recon, x)]
+        recons = [F.mse_loss(recon_, x_, reduction='mean') for recon_, x_ in zip(recon, x)]
         return torch.stack(recons).sum(dim=0)

cca_zoo/deepmodels/splitae.py
@@ -51,5 +51,5 @@ class SplitAE(_DCCA_base):
 
     @staticmethod
     def recon_loss(x, recon):
-        recons = [F.mse_loss(recon[i], x[i], reduction='sum') for i in range(len(x))]
+        recons = [F.mse_loss(recon[i], x[i], reduction='mean') for i in range(len(x))]
         return torch.stack(recons).sum(dim=0)