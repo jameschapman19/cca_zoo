5dac93a5a9987263197e91af6035e6987413a4c8
James Chapman <james.chapman.19@ucl.ac.uk>
2021-09-01 19:44:16 +0100

    Object Oriented Stuff

--- scripts/pls/example.py
+++ scripts/pls/example.py
@@ -6,6 +6,7 @@ from jax import random
 from ccagame.pls import Game, SGD, Incremental, Batch, Numpy
 # Imports
 from ccagame.pls import calc_numpy
+import time
 
 # %%
 
@@ -30,18 +31,28 @@ Y = random.normal(subkey, (n, q))
 
 # Model
 
+before = time.time()
 numpy = Numpy(scale=False, n_components=latent_dims).fit(X, Y)
 print("\n Eigenvalues calculated using numpy are :\n", numpy.score(X, Y))
+print("\n Time :\n", time.time() - before)
 
+before = time.time()
 game = Game(scale=False, lr=lr, batch_size=batch_size, epochs=epochs, n_components=latent_dims, verbose=True,
             mu=True).fit(X, Y)
 print("\n Eigenvalues calculated using game are :\n", game.score(X, Y))
+print("\n Time :\n", time.time() - before)
 
+before = time.time()
 sgd = SGD(scale=False, lr=lr, batch_size=batch_size, epochs=epochs, n_components=latent_dims, verbose=True).fit(X, Y)
 print("\n Eigenvalues calculated using sgd are :\n", sgd.score(X, Y))
+print("\n Time :\n", time.time() - before)
 
+before = time.time()
 batch = Batch(scale=False, lr=lr, epochs=epochs, n_components=latent_dims, verbose=True).fit(X, Y)
 print("\n Eigenvalues calculated using batch are :\n", batch.score(X, Y))
+print("\n Time :\n", time.time() - before)
 
+before = time.time()
 incremental = Incremental(scale=False, lr=lr, epochs=epochs, n_components=latent_dims, verbose=True).fit(X, Y)
 print("\n Eigenvalues calculated using incremental are :\n", incremental.score(X, Y))
+print("\n Time :\n", time.time() - before)
