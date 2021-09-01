f902905ef60967d24dc1fc44b6ba592d49bfd50c
James Chapman <james.chapman.19@ucl.ac.uk>
2021-09-01 11:28:44 +0100

    Added xrmb data from wang's stochastic pls and made pls methods train using batches where relevant

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ccagame',
+    version='0',
+    packages=['ccagame', 'ccagame.cca', 'ccagame.pca', 'ccagame.pls', 'ccagame.solver', 'ccagame.datasets'],
+    url='',
+    license='',
+    author='James Chapman',
+    author_email='james.chapman.19@ucl.ac.uk',
+    description='PLS/CCA/PCA formulated as games'
+)
