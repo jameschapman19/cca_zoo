fd0c9b4b1ccdb8e6346f06ca67c4b096a88345bc
alawryaguila <ana.lawryaguila@outlook.com>
2021-09-30 08:06:18 +0100

    first commit

--- ccagame/datasets/ukbiobank.py
+++ ccagame/datasets/ukbiobank.py
@@ -1,5 +1,7 @@
+from os.path import join
+import pandas as pd
 # TODO
-def ukbiobank():
+def ukbiobank(path):
     """
     Download, parse and process UKBiobank data
     Examples
@@ -12,4 +14,9 @@ def ukbiobank():
     -------
     train_view_1, train_view_2, test_view_1, test_view_2
     """
-    raise NotImplementedError
+    brain_train = pd.read_csv(join(path,'Epilepsy_MRI_train_zscores.csv'), header=None).to_numpy()
+    genetics_train = pd.read_csv(join(path,'Epilepsy_genetics_train_processed.csv'), header=None).to_numpy()
+    brain_test = pd.read_csv(join(path,'Epilepsy_MRI_test_zscores.csv'), header=None).to_numpy()
+    genetics_test = pd.read_csv(join(path,'Epilepsy_genetics_test_processed.csv'), header=None).to_numpy()
+    
+    return brain_train, genetics_train, brain_test, genetics_test
