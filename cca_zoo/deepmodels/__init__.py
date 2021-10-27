daa77a16b125031620fc384ca92a03e156d17b44
James Chapman <james.chapman.19@ucl.ac.uk>
2021-10-27 12:20:29 +0100

    Switching wrapper to pytorch-lightning

--- cca_zoo/deepmodels/__init__.py
+++ cca_zoo/deepmodels/__init__.py
@@ -4,7 +4,8 @@ from ._dcca_base import _DCCA_base
 from .dcca import DCCA
 from .dcca_noi import DCCA_NOI
 from .dccae import DCCAE
-from .deepwrapper import DeepWrapper
 from .dtcca import DTCCA
 from .dvcca import DVCCA
 from .splitae import SplitAE
+from .trainers import CCALightning
+from .utils import get_dataloaders, process_data
