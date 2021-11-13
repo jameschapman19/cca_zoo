512d2434a72ea949a0b2c175ed7c55684dfa84bb
James Chapman <james.chapman.19@ucl.ac.uk>
2021-11-13 23:49:54 +0000

    Switching tutorials to sphinx gallery

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,8 +36,8 @@ extensions = [
 
 sphinx_gallery_conf = {
     "doc_module": "cca-zoo",
-    "examples_dirs": "../examples",  # path to your example scripts
-    "gallery_dirs": "auto_examples",  # path to where to save gallery generated output
+    "examples_dirs": "../../examples",  # path to your example scripts
+    "gallery_dirs": "../auto_examples",  # path to where to save gallery generated output
 }
 
 # -- sphinx.ext.intersphinx
@@ -76,5 +76,3 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-
-master_doc = 'index'
