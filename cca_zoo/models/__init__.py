205ccb077b7f9c439b8478871789bd7dac49089f
James Chapman <56592797+jameschapman19@users.noreply.github.com>
2021-12-02 14:55:11 +0000

    Update __init__.py

--- cca_zoo/models/__init__.py
+++ cca_zoo/models/__init__.py
@@ -1,3 +1,4 @@
+from .rcca import rCCA, CCA, PLS
 from .gcca import GCCA, KGCCA
 from .iterative import (
     _Iterative,
@@ -13,5 +14,4 @@ from .iterative import (
 )
 from .mcca import MCCA, KCCA
 from .ncca import NCCA
-from .rcca import rCCA, CCA, PLS
 from .tcca import TCCA, KTCCA
