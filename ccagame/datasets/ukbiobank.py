734ab6b96843493fba71b0f884e80936d0887cd8
James <james.chapman.19@ucl.ac.uk>
2021-12-24 16:31:01 +0000

    update I hope this is OK

--- ccagame/datasets/ukbiobank.py
+++ ccagame/datasets/ukbiobank.py
@@ -11,19 +11,14 @@ import jax.numpy as jnp
 import numpy as np
 from ccagame.utils import data_stream_UKBB
 
-
-def ukbb_iterator(path, batch_size, pca=False):
-    # batch_ids = list(range(1,67)) #file naming starts at 1
-    batch_ids = list(range(1, 4))
-    # load one batch to get no. of features and use as holdout data
-    # X is brain data
-    X = pd.read_csv(join(path, "pack_1_img_sd.tab"), delimiter=" ").to_numpy().T
-    f = gzip.GzipFile(join(path, "pack_1_norm.tab.gz"), "r")
-    # Y is genetics data
-    Y = pd.read_csv(f, delimiter=" ").to_numpy().T
+def ukbb_iterator(num_batches, path, batch_size, pca=False):
+    #file naming starts at 1
+    batch_ids = list(range(1,num_batches+1)) 
+    #load one batch to get no. of features and use as holdout data
+    #X is brain data
+    X = pd.read_csv(join(path, 'pack_1_img_sd.tab'), delimiter=' ').to_numpy().T
+    f = gzip.GzipFile(join(path, 'pack_1_norm.tab.gz'), "r")
+    #Y is genetics data
+    Y = pd.read_csv(f, delimiter=" ").to_numpy().T 
     batch_ids.remove(1)
-    return (
-        data_stream_UKBB(batch_ids, path, batch_size=batch_size),
-        (X, Y),
-        (X.shape[1], Y.shape[1]),
-    )
+    return data_stream_UKBB(batch_ids, path, batch_size=batch_size), (X, Y), (X.shape[1], Y.shape[1])
