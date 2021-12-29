2f932d58f29a67ee61cc13f129b1629e059a1d04
James <james.chapman.19@ucl.ac.uk>
2021-12-29 22:08:45 +0000

    pls2

---
+++ ccagame/test/test_all.py
@@ -0,0 +1,95 @@
+from re import I
+from sys import argv
+
+from absl import flags
+from absl.testing import parameterized
+from absl.testing import absltest
+from ccagame import cca, pca, pls
+from jaxline_fork import platform
+from ml_collections import config_flags
+from ccagame.test.config import get_config
+
+FLAGS = flags.FLAGS
+
+
+class TestAll(parameterized.TestCase):
+    @parameterized.parameters(
+            {'model': 'game',
+            'model': 'oja',
+            'model': 'gha'},
+            )
+    def test_pca(self,model):
+        MODEL_DICT = {
+            "game": pca.Game,
+            "oja": pca.Oja,
+            "gha": pca.GHA,
+        }
+        flags.DEFINE_string(name="model", default="game", help="model name")
+        FLAGS.model=model
+        FLAGS.config = get_config()
+        FLAGS.config.experiment_kwargs = {
+            "n_components": FLAGS.config.n_components,
+            "num_devices": FLAGS.config.num_devices,
+            "data": FLAGS.config.data,
+            "batch_size": FLAGS.config.batch_size,
+            "learning_rate": FLAGS.config.learning_rate,
+            "validate": FLAGS.config.validate,
+        }
+        platform.main(MODEL_DICT[model], argv)
+    @parameterized.parameters(
+       {'model': 'game',
+       'model': 'appgrad',
+       'model': 'genoja',
+       'model': 'sgha',
+       'model': 'vicreggame',},
+    )
+    def test_cca(self,model):
+        MODEL_DICT = {
+            "game": cca.Game,
+            "vicreggame": cca.VicRegGame,
+            "genoja": cca.GenOja,
+            "sgha": cca.SGHA,
+            "appgrad": cca.AppGrad,
+        }
+        flags.DEFINE_string(name="model", default="game", help="model name")
+        FLAGS.model=model
+        FLAGS.config = get_config()#FLAGS.config.is_type_safe
+        FLAGS.config.experiment_kwargs = {
+            "n_components": FLAGS.config.n_components,
+            "num_devices": FLAGS.config.num_devices,
+            "data": FLAGS.config.data,
+            "batch_size": FLAGS.config.batch_size,
+            "learning_rate": FLAGS.config.learning_rate,
+            "validate": FLAGS.config.validate,
+        }
+        platform.main(MODEL_DICT[model], argv)
+    @parameterized.parameters(
+            {'model': 'game',
+            'model': 'msg',
+            'model': 'oja',
+            'model': 'power',
+            'model': 'incremental'},
+            )
+    def test_pls(self,model):
+        MODEL_DICT = {
+            "game": pls.Game,
+            "msg": pls.MSG,
+            "oja": pls.Oja,
+            "power": pls.StochasticPower,
+            "incremental": pls.Incremental,
+        }
+        flags.DEFINE_string(name="model", default="game", help="model name")
+        FLAGS.model=model
+        FLAGS.config = get_config()
+        FLAGS.config.experiment_kwargs = {
+            "n_components": FLAGS.config.n_components,
+            "num_devices": FLAGS.config.num_devices,
+            "data": FLAGS.config.data,
+            "batch_size": FLAGS.config.batch_size,
+            "learning_rate": FLAGS.config.learning_rate,
+            "validate": FLAGS.config.validate,
+        }
+        platform.main(MODEL_DICT[model], argv)
+
+if __name__ == "__main__":
+    absltest.main()
