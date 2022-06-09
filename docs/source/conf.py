793ad354ada0c52ffea76d15a7aa1a6621bcc036
James Chapman <james.chapman.19@ucl.ac.uk>
2022-06-09 12:19:25 +0100

    Permutation testing and learning curve

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,6 +57,7 @@ intersphinx_mapping = {
 
 # -- sphinx.ext.autodoc
 autodoc_member_order = "bysource"
+autoclass_content = "both"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
