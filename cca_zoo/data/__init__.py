6022ecf98027690cba28ce0182c12411834e197f
James Chapman <james.chapman.19@ucl.ac.uk>
2022-06-15 16:08:31 +0100

    Permutation testing and learning curve

--- cca_zoo/data/__init__.py
+++ cca_zoo/data/__init__.py
@@ -2,11 +2,14 @@ from .simulated import generate_covariance_data, generate_simple_data
 
 try:
     from .utils import CCA_Dataset
-except ModuleNotFoundError:
-    pass
 
-__all__ = [
-    "generate_covariance_data",
-    "generate_simple_data",
-    "CCA_Dataset",
-]
+    __all__ = [
+        "generate_covariance_data",
+        "generate_simple_data",
+        "CCA_Dataset",
+    ]
+except ModuleNotFoundError:
+    __all__ = [
+        "generate_covariance_data",
+        "generate_simple_data",
+    ]
