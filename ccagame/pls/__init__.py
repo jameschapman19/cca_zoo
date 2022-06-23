b754b52faf731482bfaa34f8c7a3c9acc1a973fe
James Chapman <james.chapman.19@ucl.ac.uk>
2022-06-23 17:54:49 +0100

    Delete ccagame/pls directory

--- ccagame/pls/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._pls import _PLS
-from .batch import Batch
-from .game import Game
-from .incremental import Incremental
-from .msg import MSG
-from .numpy import calc_numpy, Numpy
-from .sgd import SGD
