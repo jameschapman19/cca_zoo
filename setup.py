586853e30e7a8ab243f000173b2cd8761b9c0192
jameschapman19 <james.chapman.19@ucl.ac.uk>
2022-09-28 14:15:08 +0100

    Simplified install (particularly requirements.txt) to help prepare for conda. Also reorganized modules

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open("./requirements/basic.txt", "r") as f:
+with open("requirements.txt", "r") as f:
     REQUIRED_PACKAGES = f.read()
 
 EXTRA_PACKAGES = {"deep": ["torch>=1.9.0", "torchvision", "pytorch-lightning"],
                   "probabilistic": ["jax", "numpyro", "arviz"]}
-EXTRA_PACKAGES["all"] = EXTRA_PACKAGES["deep"] + "\n" + EXTRA_PACKAGES["probabilistic"]
+EXTRA_PACKAGES["all"] = EXTRA_PACKAGES["deep"] + EXTRA_PACKAGES["probabilistic"]
 
 setup(
     name="cca_zoo",
