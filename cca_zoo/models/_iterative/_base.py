Fixed normalisation across models

cca_zoo/models/_iterative/_base.py
@@ -12,6 +12,7 @@ from cca_zoo.models._dummy import _DummyCCA
 from .._mcca import MCCA, KCCA
 from cca_zoo.models._rcca import rCCA
 
+
 class _BaseIterative(_BaseCCA):
     """
     A class used as the base for _iterative CCA methods i.e. those that optimize for each dimension one at a time with deflation.
@@ -120,7 +121,7 @@ class _BaseIterative(_BaseCCA):
         # default objective is covariance
         obj = 0
         for (score_i, score_j) in combinations(scores, 2):
-            obj += np.cov(score_i,score_j)[0,1]
+            obj += np.cov(score_i, score_j)[0, 1]
         return -obj.item()
 
     def _early_stop(self, objective) -> bool:

cca_zoo/models/_iterative/_pmd.py
@@ -122,7 +122,11 @@ class SCCA_PMD(PLS_ALS):
         for view_index, view in enumerate(views):
             targets = np.ma.array(scores, mask=False)
             targets.mask[view_index] = True
-            weights[view_index] = views[view_index].T @ targets.sum(axis=0).filled()/(views[0].shape[0]-1)
+            weights[view_index] = (
+                views[view_index].T
+                @ targets.sum(axis=0).filled()
+                / (views[0].shape[0] - 1)
+            )
             weights[view_index] = _delta_search(
                 weights[view_index],
                 self.t[view_index],

cca_zoo/models/_stochastic/_eigengame.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from cca_zoo.models._stochastic._base import _BaseStochastic
 from cca_zoo.utils import _process_parameter
+from scipy.linalg import block_diag
 
 
 class CCAEigenGame(_BaseStochastic):
@@ -173,7 +174,9 @@ class CCAEigenGame(_BaseStochastic):
     def _Aw(self, views, projections):
         Aw = np.vstack(
             [
-                view.T @ projections.sum(axis=0) / projections[0].shape[0]
+                np.cov(view.T, projections.sum(axis=0).T)[
+                    0 : view.shape[1], view.shape[1] :
+                ]
                 for view in views
             ]
         )
@@ -185,9 +188,7 @@ class CCAEigenGame(_BaseStochastic):
         for i, (view, projection, weight) in enumerate(
             zip(views, projections, weights)
         ):
-            Bw.append(
-                (view.T @ projection) / (projection.shape[0]-1)
-            )
+            Bw.append((view.T @ projection) / (projection.shape[0] - 1))
         return np.vstack(Bw) / len(views)
 
     def _get_terms(self, views, u, projections=None):
@@ -225,6 +226,7 @@ class CCAEigenGame(_BaseStochastic):
 
     def _more_tags(self):
         return {"multiview": True, "stochastic": True}
+
 
 class PLSEigenGame(CCAEigenGame):
     """
@@ -327,12 +329,16 @@ class PLSEigenGame(CCAEigenGame):
     def _Aw(self, views, projections):
         Aw = np.vstack(
             [
-                view.T @ projections.sum(axis=0) / projections[0].shape[0]
+                np.cov(view, projections.sum(axis=0), rowvar=False)[
+                    0 : view.shape[1], view.shape[1] :
+                ]
                 for view in views
             ]
         ) - np.vstack(
             [
-                view.T @ projection / projections[0].shape[0]
+                np.cov(view, projection, rowvar=False)[
+                    0 : view.shape[1], view.shape[1] :
+                ]
                 for view, projection in zip(views, projections)
             ]
         )
@@ -342,7 +348,7 @@ class PLSEigenGame(CCAEigenGame):
         weights = self._split_weights(views, u)
         Bw = []
         for i, (view, projection, weight) in enumerate(
-                zip(views, projections, weights)
+            zip(views, projections, weights)
         ):
             Bw.append(weight)
         return np.vstack(Bw) / len(views)