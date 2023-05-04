Fixed normalisation across models

cca_zoo/data/simulated.py
@@ -11,16 +11,16 @@ from cca_zoo.utils import _process_parameter
 
 class LinearSimulatedData:
     def __init__(
-            self,
-            view_features: List[int],
-            latent_dims: int = 1,
-            view_sparsity: List[Union[int, float]] = None,
-            correlation: Union[List[float], float] = 0.99,
-            structure: Union[str, List[str]] = None,
-            sigma: Union[List[float], float] = None,
-            decay: float = 0.5,
-            positive=None,
-            random_state: Union[int, np.random.RandomState] = None,
+        self,
+        view_features: List[int],
+        latent_dims: int = 1,
+        view_sparsity: List[Union[int, float]] = None,
+        correlation: Union[List[float], float] = 0.99,
+        structure: Union[str, List[str]] = None,
+        sigma: Union[List[float], float] = None,
+        decay: float = 0.5,
+        positive=None,
+        random_state: Union[int, np.random.RandomState] = None,
     ):
         """
 
@@ -93,12 +93,12 @@ class LinearSimulatedData:
                 # Cross Bit
                 cross += covs[i] @ A @ covs[j]
             cov[
-            splits[i]: splits[i] + self.view_features[i],
-            splits[j]: splits[j] + self.view_features[j],
+                splits[i] : splits[i] + self.view_features[i],
+                splits[j] : splits[j] + self.view_features[j],
             ] = cross
             cov[
-            splits[j]: splits[j] + self.view_features[j],
-            splits[i]: splits[i] + self.view_features[i],
+                splits[j] : splits[j] + self.view_features[j],
+                splits[i] : splits[i] + self.view_features[i],
             ] = cross.T
         return cov
 
@@ -107,11 +107,11 @@ class LinearSimulatedData:
         covs = []
         true_features = []
         for view_p, sparsity, view_structure, view_positive, view_sigma in zip(
-                self.view_features,
-                self.view_sparsity,
-                self.structure,
-                self.positive,
-                self.sigma,
+            self.view_features,
+            self.view_sparsity,
+            self.structure,
+            self.positive,
+            self.sigma,
         ):
             cov = self._generate_covariance_matrix(view_p, view_structure, view_sigma)
             weights = self.random_state.randn(view_p, self.latent_dims)
@@ -154,12 +154,12 @@ class LinearSimulatedData:
 
 
 def simple_simulated_data(
-        n: int,
-        view_features: List[int],
-        view_sparsity: List[Union[int, float]] = None,
-        eps: float = 0,
-        transform=False,
-        random_state=None,
+    n: int,
+    view_features: List[int],
+    view_sparsity: List[Union[int, float]] = None,
+    eps: float = 0,
+    transform=False,
+    random_state=None,
 ):
     """
     Generate a simple simulated dataset with a single latent dimension
@@ -223,9 +223,9 @@ def _gaussian(x, mu, sig, dn):
     :param dn:
     """
     return (
-            np.exp(-np.power(x - mu, 2.0) / (2 * np.power(sig, 2.0)))
-            * dn
-            / (np.sqrt(2 * np.pi) * sig)
+        np.exp(-np.power(x - mu, 2.0) / (2 * np.power(sig, 2.0)))
+        * dn
+        / (np.sqrt(2 * np.pi) * sig)
     )
 
 
@@ -241,7 +241,7 @@ def _generate_gaussian_cov(p, sigma):
 
 def _generate_toeplitz_cov(p, sigma):
     c = np.arange(0, p)
-    c = sigma ** c
+    c = sigma**c
     cov = linalg.toeplitz(c, c)
     return cov
 

cca_zoo/deepmodels/_discriminative/_dcca_eigengame.py
@@ -41,6 +41,6 @@ class DCCA_EigenGame(DCCA):
                 if i == j:
                     B += torch.cov(zi.T)
                 A += torch.cov(torch.hstack((zi, zj)).T)[
-                     self.latent_dims:, : self.latent_dims
-                     ]
+                    self.latent_dims :, : self.latent_dims
+                ]
         return A / len(z), B / len(z)

cca_zoo/model_selection/_search.py
@@ -100,11 +100,11 @@ class ParameterSampler_(ParameterSampler):
                 if isinstance(v, Iterable):
                     # if each element is a distribution for each view (i.e. it is a non-string Iterable) then call return_param for each view
                     if any(
-                            [
-                                (isinstance(v_, Iterable) and not isinstance(v_, str))
-                                or hasattr(v_, "rvs")
-                                for v_ in v
-                            ]
+                        [
+                            (isinstance(v_, Iterable) and not isinstance(v_, str))
+                            or hasattr(v_, "rvs")
+                            for v_ in v
+                        ]
                     ):
                         params[k] = [self.return_param(v_) for v_ in v]
                     # if the parameter is shared across views then the list will just contain non-iterable values
@@ -160,18 +160,18 @@ class GridSearchCV(BaseSearchCV):
     _required_parameters = ["estimator", "param_grid"]
 
     def __init__(
-            self,
-            estimator,
-            param_grid,
-            *,
-            scoring=None,
-            n_jobs=None,
-            refit=True,
-            cv=None,
-            verbose=0,
-            pre_dispatch="2*n_jobs",
-            error_score=np.nan,
-            return_train_score=False,
+        self,
+        estimator,
+        param_grid,
+        *,
+        scoring=None,
+        n_jobs=None,
+        refit=True,
+        cv=None,
+        verbose=0,
+        pre_dispatch="2*n_jobs",
+        error_score=np.nan,
+        return_train_score=False,
     ):
         super().__init__(
             estimator=estimator,
@@ -208,7 +208,7 @@ class GridSearchCV(BaseSearchCV):
         self = BaseSearchCV.fit(self, np.hstack(X), y=y, groups=groups, **fit_params)
         self.best_estimator_ = self.best_estimator_["estimator"]
         self.best_params_ = {
-            key[len("estimator__"):]: val for key, val in self.best_params_.items()
+            key[len("estimator__") :]: val for key, val in self.best_params_.items()
         }
         return self
 
@@ -247,20 +247,20 @@ class RandomizedSearchCV(BaseSearchCV):
     _required_parameters = ["estimator", "param_distributions"]
 
     def __init__(
-            self,
-            estimator,
-            param_distributions,
-            *,
-            n_iter=10,
-            scoring=None,
-            n_jobs=None,
-            refit=True,
-            cv=None,
-            verbose=0,
-            pre_dispatch="2*n_jobs",
-            random_state=None,
-            error_score=np.nan,
-            return_train_score=False,
+        self,
+        estimator,
+        param_distributions,
+        *,
+        n_iter=10,
+        scoring=None,
+        n_jobs=None,
+        refit=True,
+        cv=None,
+        verbose=0,
+        pre_dispatch="2*n_jobs",
+        random_state=None,
+        error_score=np.nan,
+        return_train_score=False,
     ):
         self.param_distributions = {
             f"estimator__{key}": val for key, val in param_distributions.items()
@@ -297,6 +297,6 @@ class RandomizedSearchCV(BaseSearchCV):
         self = BaseSearchCV.fit(self, np.hstack(X), y=y, groups=groups, **fit_params)
         self.best_estimator_ = self.best_estimator_["estimator"]
         self.best_params_ = {
-            key[len("estimator__"):]: val for key, val in self.best_params_.items()
+            key[len("estimator__") :]: val for key, val in self.best_params_.items()
         }
         return self

cca_zoo/models/_iterative/_swcca.py
@@ -29,19 +29,19 @@ class SWCCA(_BaseIterative):
     """
 
     def __init__(
-            self,
-            scale: bool = True,
-            centre=True,
-            copy_data=True,
-            random_state=None,
-            max_iter: int = 500,
-            initialization: str = "random",
-            tol: float = 1e-3,
-            regularisation="l0",
-            tau: Union[Iterable[Union[float, int]], Union[float, int]] = None,
-            sample_support=None,
-            positive=False,
-            verbose=0,
+        self,
+        scale: bool = True,
+        centre=True,
+        copy_data=True,
+        random_state=None,
+        max_iter: int = 500,
+        initialization: str = "random",
+        tol: float = 1e-3,
+        regularisation="l0",
+        tau: Union[Iterable[Union[float, int]], Union[float, int]] = None,
+        sample_support=None,
+        positive=False,
+        verbose=0,
     ):
         self.tau = tau
         self.sample_support = sample_support
@@ -81,8 +81,8 @@ class SWCCA(_BaseIterative):
             targets = np.ma.array(scores, mask=False)
             targets.mask[view_index] = True
             weights[view_index] = (
-                                          views[view_index] * self.sample_weights[:, np.newaxis]
-                                  ).T @ targets.sum(axis=0).filled()
+                views[view_index] * self.sample_weights[:, np.newaxis]
+            ).T @ targets.sum(axis=0).filled()
             weights[view_index] = self.update(
                 weights[view_index],
                 self.tau[view_index],

cca_zoo/models/_stochastic/_eigengame.py
@@ -54,30 +54,30 @@ class CCAEigenGame(_BaseStochastic):
     """
 
     def __init__(
-            self,
-            latent_dims: int = 1,
-            scale: bool = True,
-            centre=True,
-            copy_data=True,
-            random_state=None,
-            tol=1e-9,
-            accept_sparse=None,
-            batch_size=None,
-            shuffle=True,
-            sampler=None,
-            batch_sampler=None,
-            num_workers=0,
-            pin_memory=False,
-            drop_last=True,
-            timeout=0,
-            worker_init_fn=None,
-            epochs=1,
-            learning_rate=1,
-            initialization: Union[str, callable] = "random",
-            momentum=0,
-            line_search=False,
-            rho=0.1,
-            ensure_descent=False,
+        self,
+        latent_dims: int = 1,
+        scale: bool = True,
+        centre=True,
+        copy_data=True,
+        random_state=None,
+        tol=1e-9,
+        accept_sparse=None,
+        batch_size=None,
+        shuffle=True,
+        sampler=None,
+        batch_sampler=None,
+        num_workers=0,
+        pin_memory=False,
+        drop_last=True,
+        timeout=0,
+        worker_init_fn=None,
+        epochs=1,
+        learning_rate=1,
+        initialization: Union[str, callable] = "random",
+        momentum=0,
+        line_search=False,
+        rho=0.1,
+        ensure_descent=False,
     ):
         super().__init__(
             latent_dims=latent_dims,
@@ -109,7 +109,7 @@ class CCAEigenGame(_BaseStochastic):
     def _split_weights(self, views, weights):
         splits = np.cumsum([0] + [view.shape[1] for view in views])
         weights = [
-            weights[split: splits[i + 1]] for i, split in enumerate(splits[:-1])
+            weights[split : splits[i + 1]] for i, split in enumerate(splits[:-1])
         ]
         return weights
 
@@ -150,7 +150,7 @@ class CCAEigenGame(_BaseStochastic):
 
             # Check if the objective function decreases with the updated solution
             if self.objective(views, u=updated_solution) < self.objective(
-                    views, u=self.weights
+                views, u=self.weights
             ) - 0.1 * step_size * np.linalg.norm(step_direction.T @ gradient):
                 break
 
@@ -173,7 +173,7 @@ class CCAEigenGame(_BaseStochastic):
         Aw = np.vstack(
             [
                 np.cov(view.T, projections.sum(axis=0).T)[
-                0: view.shape[1], view.shape[1]:
+                    0 : view.shape[1], view.shape[1] :
                 ]
                 for view in views
             ]
@@ -184,7 +184,7 @@ class CCAEigenGame(_BaseStochastic):
         weights = self._split_weights(views, u)
         Bw = []
         for i, (view, projection, weight) in enumerate(
-                zip(views, projections, weights)
+            zip(views, projections, weights)
         ):
             Bw.append((view.T @ projection) / (projection.shape[0] - 1))
         return np.vstack(Bw) / len(views)
@@ -273,30 +273,30 @@ class PLSEigenGame(CCAEigenGame):
     """
 
     def __init__(
-            self,
-            latent_dims: int = 1,
-            scale: bool = True,
-            centre=True,
-            copy_data=True,
-            random_state=None,
-            tol=1e-9,
-            accept_sparse=None,
-            batch_size=None,
-            shuffle=True,
-            sampler=None,
-            batch_sampler=None,
-            num_workers=0,
-            pin_memory=False,
-            drop_last=True,
-            timeout=0,
-            worker_init_fn=None,
-            epochs=1,
-            learning_rate=1,
-            momentum=0,
-            line_search=False,
-            rho=0.1,
-            ensure_descent=False,
-            initialization="random",
+        self,
+        latent_dims: int = 1,
+        scale: bool = True,
+        centre=True,
+        copy_data=True,
+        random_state=None,
+        tol=1e-9,
+        accept_sparse=None,
+        batch_size=None,
+        shuffle=True,
+        sampler=None,
+        batch_sampler=None,
+        num_workers=0,
+        pin_memory=False,
+        drop_last=True,
+        timeout=0,
+        worker_init_fn=None,
+        epochs=1,
+        learning_rate=1,
+        momentum=0,
+        line_search=False,
+        rho=0.1,
+        ensure_descent=False,
+        initialization="random",
     ):
         super().__init__(
             latent_dims=latent_dims,
@@ -328,14 +328,14 @@ class PLSEigenGame(CCAEigenGame):
         Aw = np.vstack(
             [
                 np.cov(view, projections.sum(axis=0), rowvar=False)[
-                0: view.shape[1], view.shape[1]:
+                    0 : view.shape[1], view.shape[1] :
                 ]
                 for view in views
             ]
         ) - np.vstack(
             [
                 np.cov(view, projection, rowvar=False)[
-                0: view.shape[1], view.shape[1]:
+                    0 : view.shape[1], view.shape[1] :
                 ]
                 for view, projection in zip(views, projections)
             ]
@@ -346,7 +346,7 @@ class PLSEigenGame(CCAEigenGame):
         weights = self._split_weights(views, u)
         Bw = []
         for i, (view, projection, weight) in enumerate(
-                zip(views, projections, weights)
+            zip(views, projections, weights)
         ):
             Bw.append(weight)
         return np.vstack(Bw) / len(views)