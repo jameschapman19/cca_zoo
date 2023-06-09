d00232883c7522ededf1281c93ded8810594579f
jameschapman19 <jameschapman19@users.noreply.github.com>
2023-06-09 15:17:53 +0000

    Format code with black

--- cca_zoo/test/test_data.py
+++ cca_zoo/test/test_data.py
@@ -4,12 +4,18 @@ Test whether linear simulated data has the correct correlations when we run cca 
 from cca_zoo.data.simulated import LinearSimulatedData
 import numpy as np
 
+
 def test_data_correlation():
-    data=LinearSimulatedData(view_features=[10,10],latent_dims=5,correlation=[0.9,0.8,0.7,0.6,0.5])
-    x_train,y_train=data.sample(1000)
-    x_test,y_test=data.sample(1000)
+    data = LinearSimulatedData(
+        view_features=[10, 10], latent_dims=5, correlation=[0.9, 0.8, 0.7, 0.6, 0.5]
+    )
+    x_train, y_train = data.sample(1000)
+    x_test, y_test = data.sample(1000)
     from cca_zoo.models import CCA
-    model=CCA(latent_dims=5)
-    model.fit((x_train,y_train))
-    assert np.allclose(model.score((x_test,y_test)),np.array([0.9,0.8,0.7,0.6,0.5]),atol=0.1)
+
+    model = CCA(latent_dims=5)
+    model.fit((x_train, y_train))
+    assert np.allclose(
+        model.score((x_test, y_test)), np.array([0.9, 0.8, 0.7, 0.6, 0.5]), atol=0.1
+    )
     print()
