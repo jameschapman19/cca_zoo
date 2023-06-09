f3bda8b6ee8426eca64eaf06fd566a43822bf464
jameschapman19 <james.chapman.19@ucl.ac.uk>
2023-06-09 15:07:15 +0100

    Version 2.0.0 changes

--- cca_zoo/plotting/__init__.py
+++ cca_zoo/plotting/__init__.py
@@ -1,13 +1,9 @@
 from .plotting import (
-    cv_plot,
     plot_pairwise_correlations,
     plot_pairwise_scatter,
-    plot_each_view_tsne,
 )
 
 __all__ = [
-    "cv_plot",
     "plot_pairwise_correlations",
     "plot_pairwise_scatter",
-    "plot_each_view_tsne",
 ]
---
+++ cca_zoo/test/test_plotting.py
@@ -0,0 +1,12 @@
+from cca_zoo.plotting import plot_pairwise_scatter, plot_pairwise_correlations
+import numpy as np
+from cca_zoo.models import CCA
+x=np.random.rand(100,10)
+y=np.random.rand(100,10)
+x-=np.mean(x,axis=0)
+y-=np.mean(y,axis=0)
+
+def test_plotting():
+    cca=CCA(latent_dims=3).fit([x,y])
+    plot_pairwise_scatter(cca,[x,y])
+    plot_pairwise_correlations(cca,[x,y])
