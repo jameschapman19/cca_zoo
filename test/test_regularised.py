e08ba097b922f2d9f3d6ec1649dafd5ee3ffe1c2
jameschapman19 <james.chapman.19@ucl.ac.uk>
2023-07-02 14:56:44 +0100

    Updated docs.

--- test/test_regularised.py
+++ test/test_regularised.py
@@ -217,11 +217,11 @@ def test_deflation():
     ccamodel.fit([X, Y])
     pls = PLS(latent_dimensions=2)
     pls.fit([X, Y])
-    plsmodel = SCCA_PMD(latent_dimensions=2, tau=0.9, random_state=rng, deflation="pls")
+    plsmodel = SCCA_PMD(latent_dimensions=2, tau=0.9, random_state=rng, deflation="pls", track='loss')
     plsmodel.fit([X, Y])
     assert (
         np.testing.assert_array_almost_equal(
-            pls.score((X, Y)), plsmodel.score((X, Y)), decimal=1
+            np.abs(pls.score((X, Y))), np.abs(plsmodel.score((X, Y))), decimal=1
         )
         is None
     )
