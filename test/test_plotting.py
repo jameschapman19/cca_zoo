069ea4f54b993fb0e76c9aeeab4d98a407f5fca8
jameschapman19 <james.chapman.19@ucl.ac.uk>
2023-09-14 14:39:56 +0100

    Adding back in explained variance and explained covariance as well as clarification of canonical loadings and loadings.

--- test/test_plotting.py
+++ test/test_plotting.py
@@ -1,3 +1,4 @@
+import pytest
 from cca_zoo.visualisation import (
     CovarianceHeatmapDisplay,
     CorrelationHeatmapDisplay,
@@ -6,43 +7,61 @@ from cca_zoo.visualisation import (
     ExplainedVarianceDisplay,
     ExplainedCovarianceDisplay,
 )
-import matplotlib.pyplot as plt
 import numpy as np
-
 from cca_zoo.linear import MCCA
+import matplotlib.pyplot as plt
 
-X = np.random.rand(100, 10)
-Y = np.random.rand(100, 10)
-Z = np.random.rand(100, 10)
 
-# Train Test Split
-X_train, X_test = X[:50], X[50:]
-Y_train, Y_test = Y[:50], Y[50:]
-Z_train, Z_test = Z[:50], Z[50:]
+@pytest.fixture(scope='module')
+def setup_data():
+    X = np.random.rand(100, 10)
+    Y = np.random.rand(100, 10)
+    Z = np.random.rand(100, 10)
 
-views = [X_train, Y_train, Z_train]
-test_views = [X_test, Y_test, Z_test]
+    X_train, X_test = X[:50], X[50:]
+    Y_train, Y_test = Y[:50], Y[50:]
+    Z_train, Z_test = Z[:50], Z[50:]
 
-# MCCA
-mcca = MCCA(latent_dimensions=2)
-mcca.fit(views)
+    views = [X_train, Y_train, Z_train]
+    test_views = [X_test, Y_test, Z_test]
 
-# Explained Variance
-ExplainedVarianceDisplay.from_estimator(mcca, views, test_views=test_views).plot()
+    mcca = MCCA(latent_dimensions=2)
+    mcca.fit(views)
 
-# Explained Covariance
-ExplainedCovarianceDisplay.from_estimator(mcca, views, test_views=test_views).plot()
+    return mcca, views, test_views
 
-# Weight heatmap
-WeightHeatmapDisplay.from_estimator(mcca).plot()
 
-# Score heatmap
-ScoreDisplay.from_estimator(mcca, views, test_views=test_views).plot()
+def test_explained_variance_plot(setup_data):
+    mcca, views, test_views = setup_data
+    ExplainedVarianceDisplay.from_estimator(mcca, views, test_views=test_views).plot()
+    plt.close()
 
-# Covariance heatmap
-CovarianceHeatmapDisplay.from_estimator(mcca, views, test_views=test_views).plot()
 
-# Correlation heatmap
-CorrelationHeatmapDisplay.from_estimator(mcca, views, test_views=test_views).plot()
+def test_explained_covariance_plot(setup_data):
+    mcca, views, test_views = setup_data
+    ExplainedCovarianceDisplay.from_estimator(mcca, views, test_views=test_views).plot()
+    plt.close()
 
-plt.show()
+
+def test_weight_heatmap_plot(setup_data):
+    mcca, _, _ = setup_data
+    WeightHeatmapDisplay.from_estimator(mcca).plot()
+    plt.close()
+
+
+def test_score_heatmap_plot(setup_data):
+    mcca, views, test_views = setup_data
+    ScoreDisplay.from_estimator(mcca, views, test_views=test_views).plot()
+    plt.close()
+
+
+def test_covariance_heatmap_plot(setup_data):
+    mcca, views, test_views = setup_data
+    CovarianceHeatmapDisplay.from_estimator(mcca, views, test_views=test_views).plot()
+    plt.close()
+
+
+def test_correlation_heatmap_plot(setup_data):
+    mcca, views, test_views = setup_data
+    CorrelationHeatmapDisplay.from_estimator(mcca, views, test_views=test_views).plot()
+    plt.close()
