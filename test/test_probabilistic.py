dd4c91b7157ea99b46c6233cdf62db9172a0aceb
jameschapman19 <jameschapman19@users.noreply.github.com>
2023-10-06 14:03:38 +0000

    Format code with black

--- test/test_probabilistic.py
+++ test/test_probabilistic.py
@@ -70,7 +70,6 @@ def test_cca_vs_probabilisticPLS(setup_data):
     ), f"Expected correlation with PLS greater than CCA, got {correlation_pls} and {correlation_cca}"
 
 
-
 def test_cca_vs_probabilisticRidgeCCA(setup_data):
     X, Y, joint = setup_data
     # Initialize models with different regularization parameters
