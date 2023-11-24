83d7a8da9c3f2e3a817aa1f05085ee58e8f6270c
jameschapman19 <james.chapman.19@ucl.ac.uk>
2023-11-24 15:08:55 +0000

    Big Update

--- examples/plot_many_views.py
+++ examples/plot_many_views.py
@@ -22,7 +22,7 @@ from cca_zoo.nonparametric import KCCA, KGCCA, KTCCA
 # Specifying the number of samples, features per view, and the latent space dimensionality.
 
 np.random.seed(42)
-n, p, q, r, latent_dims, cv = 30, 3, 3, 3, 1, 3
+n, p, q, r, latent_dimensions, cv = 30, 3, 3, 3, 1, 3
 
 (X, Y, Z) = JointData(
     view_features=[p, q, r], latent_dimensions=latent_dimensions, correlation=[0.9]
@@ -35,10 +35,10 @@ n, p, q, r, latent_dims, cv = 30, 3, 3, 3, 1, 3
 # to find the optimal linear transformations for the representations to maximize correlation.
 
 # MCCA (Multiset CCA) - Generalizes CCA for multiple representations by maximizing pairwise correlations.
-mcca = MCCA(latent_dimensions=latent_dims).fit((X, Y, X)).score((X, Y, Z))
+mcca = MCCA(latent_dimensions=latent_dimensions).fit((X, Y, X)).score((X, Y, Z))
 
 # GCCA (Generalized CCA) - Maximizes correlation between each transformed view and a shared latent variable.
-gcca = GCCA(latent_dimensions=latent_dims).fit((X, Y, X)).score((X, Y, Z))
+gcca = GCCA(latent_dimensions=latent_dimensions).fit((X, Y, X)).score((X, Y, Z))
 
 # %%
 # Kernel Methods
@@ -47,10 +47,10 @@ gcca = GCCA(latent_dimensions=latent_dims).fit((X, Y, X)).score((X, Y, Z))
 # and then apply linear transformations in that space.
 
 # KCCA (Kernel CCA) - Kernel-based extension of CCA for multiple representations.
-kcca = KCCA(latent_dimensions=latent_dims).fit((X, Y, X)).score((X, Y, Z))
+kcca = KCCA(latent_dimensions=latent_dimensions).fit((X, Y, X)).score((X, Y, Z))
 
 # KGCCA (Kernel Generalized CCA) - A kernel-based version of GCCA for multiple representations.
-kgcca = KGCCA(latent_dimensions=latent_dims).fit((X, Y, X)).score((X, Y, Z))
+kgcca = KGCCA(latent_dimensions=latent_dimensions).fit((X, Y, X)).score((X, Y, Z))
 
 # %%
 # Iterative Techniques
@@ -59,7 +59,7 @@ kgcca = KGCCA(latent_dimensions=latent_dims).fit((X, Y, X)).score((X, Y, Z))
 
 # SPLS (Sparse CCA by Penalized Matrix Decomposition) - A sparse CCA variant.
 pmd = (
-    SPLS(latent_dimensions=latent_dims, tau=0.1, tol=1e-5)
+    SPLS(latent_dimensions=latent_dimensions, tau=0.1, tol=1e-5)
     .fit((X, Y, X))
     .score((X, Y, Z))
 )
@@ -70,7 +70,7 @@ pmd = (
 # Techniques utilizing tensor decomposition to discern higher-order correlations among the representations.
 
 # TCCA (Tensor CCA) - A tensor-based extension of CCA for multiple representations.
-tcca = TCCA(latent_dimensions=latent_dims).fit((X, Y, X)).score((X, Y, Z))
+tcca = TCCA(latent_dimensions=latent_dimensions).fit((X, Y, X)).score((X, Y, Z))
 
 # KTCCA - [Provide a brief description, as it's missing in the original].
-ktcca = KTCCA(latent_dimensions=latent_dims).fit((X, Y, X)).score((X, Y, Z))
+ktcca = KTCCA(latent_dimensions=latent_dimensions).fit((X, Y, X)).score((X, Y, Z))
