d36ef83dba446ab3a5048f540b3819ee6542a3a1
jameschapman19 <jameschapman19@users.noreply.github.com>
2023-11-24 19:05:52 +0000

    Format code with black

--- cca_zoo/linear/__init__.py
+++ cca_zoo/linear/__init__.py
@@ -2,7 +2,14 @@
 Linear CCA methods
 """
 from ._gcca import GCCA
-from ._gradient import CCA_EY, CCA_GHA, CCA_SVD, CCA_TraceNorm, PLS_EY, PLSStochasticPower
+from ._gradient import (
+    CCA_EY,
+    CCA_GHA,
+    CCA_SVD,
+    CCA_TraceNorm,
+    PLS_EY,
+    PLSStochasticPower,
+)
 from ._grcca import GRCCA
 from ._iterative import (
     PLS_ALS,
